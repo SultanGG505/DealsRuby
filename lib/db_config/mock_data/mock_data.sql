e0af6e25cc5912de23ca6e2fb7dfb4cedc3f58d1
Sultan <73115468+SultanGG505@users.noreply.github.com>
2023-05-17 14:33:09 +0300

    Update mock_data.sql
    даю sql-ю заполнять поля самостоятельно

--- lib/db_config/mock_data/mock_data.sql
+++ lib/db_config/mock_data/mock_data.sql
@@ -2,20 +2,20 @@ USE Sales;
 
 
 -- Заполнение таблицы "Товары"
-INSERT INTO Product (product_id, name, price, delivery)
-VALUES (1, 'Product 1', 10.99, 'In Stock'),
-       (2, 'Product 2', 19.99, 'Out of Stock'),
-       (3, 'Product 3', 5.99, 'In Stock');
+INSERT INTO Product (name, price, delivery)
+VALUES ('Product 1', 10.99, 'In Stock'),
+       ('Product 2', 19.99, 'Out of Stock'),
+       ('Product 3', 5.99, 'In Stock');
 
 -- Заполнение таблицы "Заказчики"
-INSERT INTO Customer (customer_id, company_name, address, email)
-VALUES (1, 'Company 1300', 'Address 1', 'email1@example.com'),
-       (2, 'Company 2', 'Address 2', 'email2@example.com'),
-       (3, 'Company 3', 'Address 3', 'email3@example.com');
+INSERT INTO Customer (company_name, address, email)
+VALUES ('Company 1300', 'Address 1', 'email1@example.com'),
+       ('Company 2', 'Address 2', 'email2@example.com'),
+       ('Company 3', 'Address 3', 'email3@example.com');
 
 -- Заполнение таблицы "Сделки"
-INSERT INTO Deal (deal_id, customer_id, product_id, quantity, purchase_date)
-VALUES (1, 1, 1, 5, '2023-01-01'),
-       (2, 2, 1, 2, '2023-02-15'),
-       (3, 2, 3, 10, '2023-03-10'),
-       (4, 3, 2, 3, '2023-04-20');
+INSERT INTO Deal (customer_id, product_id, quantity, purchase_date)
+VALUES (1, 1, 5, '2023-01-01'),
+       (2, 1, 2, '2023-02-15'),
+       (2, 3, 10, '2023-03-10'),
+       (3, 2, 3, '2023-04-20');
