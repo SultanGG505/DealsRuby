87c744aca28b12782a808177aedf4e2b65479631
Sultan <73115468+SultanGG505@users.noreply.github.com>
2023-05-18 01:19:14 +0300

    Update Deal.rb
    обновил модель Сделка

--- lib/models/Deal.rb
+++ lib/models/Deal.rb
@@ -4,10 +4,21 @@ class Deal
   attr_reader :id, :quantity, :customer_id, :product_id, :purchase_date
 
   def initialize(id, purchase_date, quantity, customer_id, product_id)
+    validate_all_null(purchase_date, quantity, customer_id, product_id)
     @id = id
     @purchase_date = purchase_date
     @quantity = quantity
     @customer_id = customer_id
     @product_id = product_id
+  end
+
+  private
+
+  def validate_all_null(*args)
+    args.each do |arg|
+      if arg.nil?
+        raise ArgumentError, "Argument '#{caller_locations(1, 1)[0].label}' cannot be null"
+      end
+    end
   end
 end
