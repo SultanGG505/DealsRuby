ac074dabd496d3502787ae711c787aab9891e7d6
Sultan <73115468+SultanGG505@users.noreply.github.com>
2023-05-18 01:16:52 +0300

    Update Product.rb
    обновил модель Товар

--- lib/models/Product.rb
+++ lib/models/Product.rb
@@ -2,10 +2,22 @@
 
 class Product
   attr_reader :id, :name, :price, :delivery
-  def initialize(id, name, price, delivery)
+
+  def initialize(id, name, price, delivery = nil)
+    validate_all_null(name, price)
     @id = id
     @name = name
     @price = price
     @delivery = delivery
+  end
+
+  private
+
+  def validate_all_null(*args)
+    args.each do |arg|
+      if arg.nil?
+        raise ArgumentError, "Argument '#{caller_locations(1, 1)[0].label}' cannot be null"
+      end
+    end
   end
 end
